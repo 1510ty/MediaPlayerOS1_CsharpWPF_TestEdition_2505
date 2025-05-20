46aae606102679cd9cf6a3f9461f78032e639777
1510ty <1510ty2580@gmail.com>
2025-05-20 19:49:43 +0900

    プロジェクト ファイルを追加します。

---
+++ MediaPlayerOS1_CsharpWPF_TestEdition_2505/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace MediaPlayerOS1_CsharpWPF_TestEdition_2505
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
